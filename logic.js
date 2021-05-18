3694c1e53aea757e5df2a9f4a8a2c15b750fe9bc
Dybondzy <79281335+Dybondzy@users.noreply.github.com>
2021-05-17 23:12:59 -0400

    Add files via upload

--- logic.js
+++ logic.js
@@ -3,15 +3,67 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
-// Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//  center: [
-//    40.7, -94.5
-//  ],
-//  zoom: 4
-// });
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data).addTo(map);
+});
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+  L.geoJson(data, {
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup();
+     }
+}).addTo(map);
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
+
+
+//  Add a marker to the map for Los Angeles, California.
+L.circle([34.0522, -118.2437], {
+  radius: 100
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -25,6 +77,63 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+//  Add a marker to the map for Los Angeles, California.
+// An array containing each city's location, state, and population.
+let cities = [{
+  location: [40.7128, -74.0059],
+  city: "New York City",
+  state: "NY",
+  population: 8398748
+},
+{
+  location: [41.8781, -87.6298],
+  city: "Chicago",
+  state: "IL",
+  population: 2705994
+},
+{
+  location: [29.7604, -95.3698],
+  city: "Houston",
+  state: "TX",
+  population: 2325502
+},
+{
+  location: [34.0522, -118.2437],
+  city: "Los Angeles",
+  state: "CA",
+  population: 3990456
+},
+{
+  location: [33.4484, -112.0740],
+  city: "Phoenix",
+  state: "AZ",
+  population: 1660272
+}
+];
+
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.marker(city.location)
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+.addTo(map);
+});
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.marker(city.location).addTo(map);
+});
+
+L.circle([34.0522, -118.2437], {
+   radius: 100
+}).addTo(map);
+
+// L.circleMarker([34.0522, -118.2437]).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {

0403a7d3097d90594f64d5bba1eb8719e226d9d4
Dybondzy <79281335+Dybondzy@users.noreply.github.com>
2021-05-18 00:36:36 -0400

    Add files via upload

--- logicStep1.js
+++ logicStep1.js
@@ -21,6 +21,21 @@ let baseMaps = {
   Light: light
 };
 
+
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
+
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
+
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
   center: [30, 30],
